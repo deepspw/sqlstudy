7b07300649ec7283856a346e7453c55238f09f07
ttye <tomball.me@icloud.com>
2015-07-01 05:47:02 +0100

    fixed database flaws

--- forumdb.py
+++ forumdb.py
@@ -4,9 +4,7 @@
 
 import time
 import psycopg2
-## Database connection
-
-
+import bleach
 
 ## Get posts from database.
 def GetAllPosts():
@@ -33,6 +31,7 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
+    content = bleach.clean(content)
     connection = psycopg2.connect('dbname=forum')
     c = connection.cursor()
     c.execute("INSERT INTO posts(content) VALUES (%s)", (content,))
